31761148d92026c68d79b16029498a893749555a
hershey-ss007 <hm4585@srmist.edu.in>
2026-03-11 08:51:06 +0530

    Finished level 2 problem 1

---
+++ Level2_1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class Level2_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        double number1 = sc.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double number2 = sc.nextDouble();
+
+        double addition = number1 + number2;
+        double subtraction = number1 - number2;
+        double multiplication = number1 * number2;
+        double division = number1 / number2;
+
+        System.out.println(
+            "The addition, subtraction, multiplication, and division value of 2 numbers "
+            + number1 + " and " + number2 + " is "
+            + addition + ", " + subtraction + ", " + multiplication + ", and " + division
+        );
+
+        sc.close();
+    }
+}
